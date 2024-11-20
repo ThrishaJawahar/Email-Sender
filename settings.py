115aa91961235f0e37d1ae061631ebae58d3b17d
ThrishaJawahar <thrishajawahar@gmail.com>
2024-11-20 18:22:40 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,2 @@
+In settings.py, register ‘Emailapp’ in INSTALLED_APPS=[].
+Also register the Email related details like EMAIL_BACKEND, EMAIL_HOST, EMAIL_HOST_USER, EMAIL_PORT, EMAIL_HOST_PASSWORD and EMAIL_USE_TLS.
