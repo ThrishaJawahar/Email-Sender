5758d26f322402eec98e306230c1f39d5771693b
ThrishaJawahar <thrishajawahar@gmail.com>
2024-11-20 18:21:28 +0530

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from Emailapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('send_email/', views.send_email, name='send_email'),
+]
